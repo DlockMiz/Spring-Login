99ffcc986ea2b7e0a105b206f670dcaf78c5f5ff
Daniel <daniel.w.lock@gmail.com>
2020-09-10 18:10:21 -0500

    Added Role Functionality

--- src/main/java/project/school/springreacttemplate/Repositories/ReservationRepository.java
+++
@@ -1,6 +0,0 @@
-package project.school.springreacttemplate.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.school.springreacttemplate.Models.ReservationData.Reservations;
-
-public interface ReservationRepository extends JpaRepository<Reservations, Integer> {}
